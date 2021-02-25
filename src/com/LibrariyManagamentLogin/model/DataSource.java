3405a2d1357026a70600012ad9afd85804f9f0fd
Metin Aytekin <metinaytekin17@gmail.com>
2021-02-26 02:11:52 +0300

    .

--- src/com/LibrariyManagamentLogin/model/DataSource.java
+++ src/com/LibrariyManagamentLogin/model/DataSource.java
@@ -1,5 +1,8 @@
 package com.LibrariyManagamentLogin.model;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class DataSource {
     private static DataSource instance;
 
@@ -8,6 +11,52 @@ public class DataSource {
     private static DataSource getInstance(){
         return instance;
     }
+
+    public static final String DB_NAME = "kutuphane";
+    public static final String driver="com.mysql.cj.jdbc.Driver";
+    public static final String url="jdbc:mysql://localhost/kutuphane";
+    public static final String user="root";
+    public static final String password="1234";
+
+
+    //TABLOLAR
+    //ADRSLER
+    public static final String TABLE_ADRESLER="album";
+    public static final String SUTUN_ADRES_NO="adres_no";
+    public static final String SUTUN_BINA_NO="bina_no";
+    public static final String SUTUN_CADDE="cadde";
+    public static final String SUTUN_MAHALLE="mahalle";
+    public static final String SUTUN_SEHIR="sehir";
+    public static final String SUTUN_POSTA_KODU="posta_kodu";
+    public static final String SUTUN_ULKE="ulke";
+
+    //EMANET
+    public static final String TABLE_EMANET="emanet";
+    public static final String SUTUN_EMANET_NO="emanet_no";
+    public static final String SUTUN_EMANET_ISBN="isbn";
+    public static final String SUTUN_UYE_NO="uye_no";
+    public static final String SUTUN_KUTUPHANE_NO="kutuphane_no";
+    public static final String SUTUN_EMANET_TARIHI="emanet_tarihi";
+    public static final String SUTUN_TESLIM_TARIHI="teslim_tarihi";
+
+    //KATEGORİLER
+    public static final String TABLE_KATEGORILER="kategoriler";
+    public static final String SUTUN_KATEGORI_ADI="kategori_adi";
+
+    //KITAPLAR
+    public static final String TABLE_KITAPLAR="kitaplar";
+    public static final String SUTUN_KITAPLAR_ISBN="isbn";
+    public static final String SUTUN_KITAP_ADI="kitap_adi";
+    public static final String SUTUN_YAYIN_TARIHI="yayin_tarihi";
+    public static final String SUTUN_SAYFA_SAYISI="sayfa_sayisi";
+
+
+    public static final int SIRALA_ARTAN=1;
+    public static final int SIRALA_AZALAN=2;
+
+
+    private Connection connection = null ;
+    private Statement statement = null;
 
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module LibraryManagamentSystem.view {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
     opens com.LibrariyManagamentLogin;
 }
