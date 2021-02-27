ebdee4dda5567da38beb8c1eae0226f8a5ff4fb4
furkancetkin <furkan.cetkin@hotmail.com>
2021-02-27 21:12:33 +0300

    Kitaplar listesinde ekle, sil, güncelle

--- src/com/LibrariyManagamentLogin/Controller.java
+++ src/com/LibrariyManagamentLogin/Controller.java
@@ -1,12 +1,18 @@
 package com.LibrariyManagamentLogin;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,6 +38,16 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        /*loginbtn.setOnAction(actionEvent -> {
+            try {
+                Parent rootNode = FXMLLoader.load(getClass().getResource("yonetici.fxml"));
+                Stage stage = new Stage();
+                Scene scene = new Scene(rootNode);
+                stage.setScene(scene);
+                stage.showAndWait();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });*/
     }
 }
